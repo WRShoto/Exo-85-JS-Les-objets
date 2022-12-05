a7379fca5f496e658f9b64a1fbace8926875a110
benji <Benji59btw@gmail.com>
2022-12-05 13:59:04 +0100

    exo 85 benjamin

--- Exo 3/exercice3.js
+++ Exo 3/exercice3.js
@@ -1,13 +1,14 @@
 let personne = {
     "prenom" : "James",
-    "nom" : "Bond",
-           :    ,
-            :
+    "nom" : "Bond ",
+    "metier": "agent secret",
+    "Nom de code": "007",
+
 };
 
-/* Afficher ci dessous "mon nom est <propriété nom de notre objet>, <propriété prenom de notre objet>
+/* Afficher ci-dessous "mon nom est <propriété nom de notre objet>, <propriété prenom de notre objet>
  <propriété nom de notre objet>, agent <propriété nomDeCode de notre objet>" dans un alert */
 
-alert();
+alert("mon nom est "+ personne["nom"] + personne["prenom"]+", agent " + personne["Nom de code"]);
 
 
